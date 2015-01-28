dbc9b392fa84035d9cf47cf8bbab16adddbe4e9a
Channing Walton <channingwalton@mac.com>
2015-01-28 22:13:22 +0000

    added a texture

--- core/src/main/scala/com/casualmiracles/rxlift/rxhtml.scala
+++ core/src/main/scala/com/casualmiracles/rxlift/rxhtml.scala
@@ -2,16 +2,29 @@ package com.casualmiracles.rxlift
 
 import java.util.UUID
 
+import net.liftweb.http.SHtml
 import net.liftweb.http.js.{JsCmds, JsCmd}
-import rx.lang.scala.Observable
+import rx.lang.scala.{Subject, Observable}
+import net.liftweb.http.SHtml.ElemAttr._
 
 object RxHtml {
 
-  def label: Component[String, Nothing] = Component { (in: Observable[String]) ⇒
-    val id = UUID.randomUUID().toString
-    val jsout: Observable[JsCmd] = in.map(v ⇒ JsCmds.SetValById(id, v))
+  private def genId: String = UUID.randomUUID().toString
+
+  def label: Component[String, String] = Component { (in: Observable[String]) ⇒
+    val id = genId
+    val js: Observable[JsCmd] = in.map(v ⇒ JsCmds.SetValById(id, v))
 
     // a label does not emit a value so Out.values is empty
-    Out(Observable.empty, jsout, <span id={id}></span>, Some(id))
+    Out(Observable.empty, js, <span id={id}></span>, Some(id))
+  }
+
+  def textarea: Component[String, String] = Component { (in: Observable[String]) ⇒
+    val id = genId
+    val subject = Subject[String]()
+    val ui = SHtml.ajaxText("", subject.onNext(_), "id" → id)
+    val js: Observable[JsCmd] = in.map(v ⇒ JsCmds.SetValById(id, v))
+
+    Out(subject, js, ui, Some(id))
   }
 }
