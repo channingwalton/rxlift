39044c178fff966eeec0b3dfe8a63c576b93b782
Channing Walton <channingwalton@mac.com>
2015-01-28 23:48:48 +0000

    improved the documentation in the Clock example

--- example/src/main/scala/code/comet/Clock.scala
+++ example/src/main/scala/code/comet/Clock.scala
@@ -16,13 +16,22 @@ class Clock extends CometActor {
 
   override def defaultPrefix = Full("clk")
 
+  // Here is an Observable that will generate a string containing the time every 2 seconds
   val ticker: Observable[String] = Observable.interval(Duration(2, TimeUnit.SECONDS)).map(_ ⇒ new Date().toString)
-  val timeLabel: Out[String] = label.run(ticker)
-  timeLabel.jscmd.map(this ! _).subscribe()
 
+  // construct a label with the ticker
+  val timeLabel: Out[String] = label.run(ticker)
+
+  // send the JsCmds emitted by the label to the actor to send to the UI
+  val subscription = timeLabel.jscmd.map(this ! _).subscribe()
+
+  // initial render uses the label's ui
   def render = bind("time" -> timeLabel.ui)
 
   override def lowPriority : PartialFunction[Any, Unit] = {
     case cmd: JsCmd ⇒ partialUpdate(cmd)
   }
+
+  // important to unsubscribe to subscriptions
+  override def localShutdown() = subscription.unsubscribe()
 }
