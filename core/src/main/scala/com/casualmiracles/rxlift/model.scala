3942b3d8eb123b05ce076c37f379f88b2e75f933
Channing Walton <channingwalton@mac.com>
2015-01-28 22:00:13 +0000

    added Semigroup[Component]

--- core/src/main/scala/com/casualmiracles/rxlift/model.scala
+++ core/src/main/scala/com/casualmiracles/rxlift/model.scala
@@ -4,7 +4,19 @@ import net.liftweb.http.js.JsCmd
 import rx.lang.scala.Observable
 
 import scala.xml.NodeSeq
+import scalaz.Semigroup
 
 case class Out[T](values: Observable[T], jscmd: Observable[JsCmd], ui: NodeSeq, id: Option[String])
 
 case class Component[I, O](run: Observable[I] ⇒ Out[O])
+
+object Component {
+  implicit def ComponentSemiGroup[I, O]: Semigroup[Component[I, O]] = new Semigroup[Component[I, O]] {
+    override def append(f1: Component[I, O], f2: ⇒ Component[I, O]): Component[I, O] =
+      Component { in ⇒
+        val o1: Out[O] = f1.run(in)
+        val o2: Out[O] = f2.run(in)
+        Out(o1.values.merge(o2.values), o1.jscmd.merge(o2.jscmd), o1.ui ++ o2.ui, None)
+      }
+  }
+}
