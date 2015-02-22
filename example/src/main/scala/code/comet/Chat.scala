9f865338fa3e7ff98eae8ba9be11cec2e4b589fd
Channing Walton <channingwalton@mac.com>
2015-02-22 11:39:56 +0000

    making things clearer

--- example/src/main/scala/code/comet/Chat.scala
+++ example/src/main/scala/code/comet/Chat.scala
@@ -1,7 +1,7 @@
 package code.comet
 
 import com.casualmiracles.rxlift.Components._
-import com.casualmiracles.rxlift.{RxCometActor, RxElement}
+import com.casualmiracles.rxlift.{RxComponent, RxCometActor, RxElement}
 import net.liftweb.common.Full
 import net.liftweb.http.js.JsCmds.Focus
 
@@ -20,32 +20,31 @@ class Chat extends RxCometActor {
   override def defaultPrefix = Full("chat")
 
   // whose talking?
-  val username = text().consume(Observable.empty)
+  val username: RxElement[String] = text().consume(Observable.empty)
 
   // make an initially disabled text component, enabled by the username being set
-  val msgEditable = Observable.just(false).merge(username.values.map(_.trim.nonEmpty))
-  val editableMsg = editable(text(), msgEditable)
+  val msgEditable: Observable[Boolean] = Observable.just(false).merge(username.values.map(_.trim.nonEmpty))
+  val editableMsg: RxComponent[String, String] = editable(text(), msgEditable)
 
   // run the editableMsg with an input observable used below to reset the input field
-  val msgIn = Subject[String]()
-  val msg = editableMsg.consume(msgIn)
+  val msgIn: Subject[String] = Subject[String]()
+  val msg: RxElement[String] = editableMsg.consume(msgIn)
 
   // combine the username and messages and map it to a Message
-  val messages = username.values.combineLatest(msg.values).map{ case (u, m) ⇒ Message(u, m) }
+  val messages: Observable[Message] = username.values.combineLatest(msg.values).map{ case (u, m) ⇒ Message(u, m) }
 
   // send the user's message to the 'chat server' and blank the msg field
-  val messageDistributor = messages.filter(_.msg.trim.nonEmpty).map(m ⇒ {
+  handleSubscription(messages.filter(_.msg.trim.nonEmpty).map(m ⇒ {
     Chat.send.onNext(m)
     msgIn.onNext("")
-    this ! Focus(msg.id)
-  })
+    focusOnMsg()
+  }))
+
+  def focusOnMsg(): Unit = this ! Focus(msg.id)
 
   // a textarea whose content is obtained from Chat.messages
   def msgLine(msgs: Seq[Message]): String = msgs.map(m ⇒ m.username + ": " + m.msg).mkString("\n")
   val allMessages: RxElement[String] = textArea().consume(Chat.allMessages.map(msgLine))
-
-  // subscribe to the distributor
-  handleSubscription(messageDistributor)
 
   publish(allMessages, username, msg)
 
