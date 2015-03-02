19f86a9b5ade8c6626181fd72145fd2bb98502c7
Channing Walton <channingwalton@mac.com>
2015-03-02 22:09:04 +0000

    simplified the RxCometActor

--- core/src/main/scala/com/casualmiracles/rxlift/RxCometActor.scala
+++ core/src/main/scala/com/casualmiracles/rxlift/RxCometActor.scala
@@ -7,17 +7,17 @@ import scala.collection.mutable.ListBuffer
 
 trait RxCometActor extends CometActor {
 
+  // set of Rx subscriptions that need to be unsubscribed to when the actor dies
   val subscriptions: ListBuffer[Subscription] = ListBuffer.empty[Subscription]
-  
-  def publish(components: RxElement[_]*): Unit = {
-    components.foreach(o ⇒ handleSubscription(o.jscmd.map(this ! _)))
-  }
 
+  // publish each RxElement's jscmd by sending values from the stream to
+  // this actor for sending to the client
+  def publish(components: RxElement[_]*): Unit =
+    components.foreach(o ⇒ handleSubscription(o.jscmd.map(partialUpdate(_))))
+
+  // convenient method to subscribe to an Observable and manage the subscription 
   def handleSubscription[T](obs: Observable[T]): Unit = subscriptions += obs.subscribe()
 
-  final override def lowPriority : PartialFunction[Any, Unit] = {
-    case cmd: JsCmd ⇒ partialUpdate(cmd)
-  }
-
+  // unsubscribe to all subscriptions
   override def localShutdown() = subscriptions.foreach(_.unsubscribe())
 }
